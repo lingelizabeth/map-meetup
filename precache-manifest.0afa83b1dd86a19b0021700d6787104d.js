b3cf09dbdb85620e0292165c341ae2b41febea2a
Elizabeth <wuhanda12@yahoo.com>
2020-07-28 13:28:06 -0400

    Updates

--- precache-manifest.0afa83b1dd86a19b0021700d6787104d.js
+++ precache-manifest.0afa83b1dd86a19b0021700d6787104d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3cb5e07b8f4f771f3d3b3a1eeda21a8b",
+    "revision": "2e4d3e6d4eee9ffe2e7d64e83a156081",
     "url": "/map-meetup/index.html"
   },
   {
-    "revision": "0694ada82065aba1f88d",
+    "revision": "7313bd2d33aa9b33e422",
     "url": "/map-meetup/static/css/main.ab442475.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/map-meetup/static/js/2.59aa4927.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0694ada82065aba1f88d",
-    "url": "/map-meetup/static/js/main.21d3e82f.chunk.js"
+    "revision": "7313bd2d33aa9b33e422",
+    "url": "/map-meetup/static/js/main.789c84ed.chunk.js"
   },
   {
     "revision": "4888409152511b3d139d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/map-meetup/precache-manifest.1bab388a0aa9c81f20e72e609fb53b62.js"
+  "/map-meetup/precache-manifest.0afa83b1dd86a19b0021700d6787104d.js"
 );
 
 self.addEventListener('message', (event) => {
