c8a707a33505ef1ae8d839265f80325b1d53fddd
Elizabeth <wuhanda12@yahoo.com>
2020-08-14 14:41:05 -0400

    Updates

--- precache-manifest.9c03400a49924e1fa9eaa392e3a9afc5.js
+++ precache-manifest.9c03400a49924e1fa9eaa392e3a9afc5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1224a8ea18a7b63f176772afadb965b2",
+    "revision": "acd15f5e482b3b377cb95c9f69cd0854",
     "url": "/map-meetup/index.html"
   },
   {
-    "revision": "6b4e336a00885b948009",
+    "revision": "6585209faffd1f16d83f",
     "url": "/map-meetup/static/css/main.ab442475.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/map-meetup/static/js/2.6cdf0592.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6b4e336a00885b948009",
-    "url": "/map-meetup/static/js/main.18666326.chunk.js"
+    "revision": "6585209faffd1f16d83f",
+    "url": "/map-meetup/static/js/main.061d713b.chunk.js"
   },
   {
     "revision": "4888409152511b3d139d",
